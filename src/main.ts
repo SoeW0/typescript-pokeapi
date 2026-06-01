e6a32f2ca3fc6cc533276b38ed25790028ed0336
Pedro <pa89218@gmail.com>
2026-06-01 20:35:59 -0300

    Implementation of Pokemon Interface

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("Initial Commit")
+import * as fs from "fs";
+import * as path from "path";
+
+//Pokemon Interface
+interface pokemon {
+  id: number,
+  name: string,
+  type: string[],
+  height: number,
+  weight: number
+}
